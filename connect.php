f6bff80306f736df25dec4401f93985bcb1d3963
neoilhamakbar <50211523+neoilhamakbar@users.noreply.github.com>
2019-11-04 03:58:35 +0700

    Update connect.php

--- connect.php
+++ connect.php
@@ -1,8 +1,8 @@
 <?php
-	$host = "13.76.171.120";
+	$host = "127.0.0.1";
 	$user = "postgres";
 	$pass = "neo@123456789";
-	$port = "3389";
+	$port = "5432";
 	$dbname = "bkt_tourism";
 	$conn = pg_connect("host=".$host." port=".$port." dbname=".$dbname." user=".$user." password=".$pass) or die("Gagal");
 ?>
