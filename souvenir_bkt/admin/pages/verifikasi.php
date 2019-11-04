75f4b6ac561701b980f795f728866fc5e21705ad
neoilhamakbar <50211523+neoilhamakbar@users.noreply.github.com>
2019-11-04 10:37:26 +0700

    Update verifikasi.php

--- souvenir_bkt/admin/pages/verifikasi.php
+++ souvenir_bkt/admin/pages/verifikasi.php
@@ -4,5 +4,5 @@ include ('../../../connect.php');
 $edit = pg_query("update admin set role='C' where username='$_GET[user]'");
 
 if($edit){
-	header('location:http://localhost:8080/kuliner_bkt/index.php');
+	header('location:http://souvenirwebgis.ddns.net/bkt_tourism/index.php');
 }
