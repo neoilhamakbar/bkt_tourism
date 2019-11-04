d41418f78b8b970fb99bd159e850cf6cc1281881
neoilhamakbar <50211523+neoilhamakbar@users.noreply.github.com>
2019-11-04 10:36:20 +0700

    Update connect.php

--- souvenir_bkt/admin/inc/connect.php
+++ souvenir_bkt/admin/inc/connect.php
@@ -1,7 +1,7 @@
 <?php
 	$host = "localhost";
 	$user = "postgres";
-	$pass = "12345";
+	$pass = "neo@123456789";
 	$port = "5432";
 	$dbname = "bkt_tourism";
 	$conn = pg_connect("host=".$host." port=".$port." dbname=".$dbname." user=".$user." password=".$pass) or die("Gagal");
